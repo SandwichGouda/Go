412390435e6cbd0bd7c06830df0e534ba5689a37
SandwichGouda <william.driot@gmail.com>
2025-02-19 14:41:13 +0100

    Project Euler, another solution

--- ProjectEuler/Problem4.go
+++ ProjectEuler/Problem4.go
@@ -57,6 +57,60 @@ func LargestPalindrome(n int) (m int) {
 	return
 }
 
+func Reverse(s []int) []int {
+
+	l := len(s)
+
+	r := make([]int, l, l)
+
+	for k := 0; k < l; k++ {
+		r[l-k-1] = s[k]
+	}
+	return r
+}
+
+func IntToSlice(n int) []int {
+	nb := NbDigits(n)
+	var d int
+	sl := make([]int, nb, nb)
+
+	for i := 0; i <= nb-1; i++ {
+		d = n % 10
+		sl[nb-i-1] = d
+		n = (n - d) / 10
+	}
+
+	return sl
+}
+
+func IsPalindrome2(n int) (b bool) {
+	b = true
+	sl := IntToSlice(n)
+	r := Reverse(sl)
+	l := len(sl)
+
+	for k := 0; k < l; k++ {
+		if sl[k] != r[k] {
+			b = false
+		}
+	}
+	return
+}
+
+func LargestPalindrome2(n int) (m int) {
+
+	for a := Pow(10, n-1); a < Pow(10, n); a++ {
+		for b := Pow(10, n-1); b < Pow(10, n); b++ {
+			if a*b > m {
+				if IsPalindrome2(a * b) {
+					m = a * b
+				}
+			}
+		}
+	}
+	return
+}
+
 func main() {
 	fmt.Println(LargestPalindrome2(3))
 }
