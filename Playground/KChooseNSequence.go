fd227942431df36db3cfb8d8f7c485a00c89313c
SandwichGouda <william.driot@gmail.com>
2025-07-08 19:23:45 +0200

    Found a counterexample for IMO 2019 Shortlist problem C1

--- Playground/KChooseNSequence.go
+++ Playground/KChooseNSequence.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"log"
 )
 
 func pascalsTriangle(n int) [][]int {
@@ -24,7 +25,48 @@ func pascalsTriangle(n int) [][]int {
 	return pT
 }
 
+
+
 func main() {
 	n, _ := strconv.Atoi(os.Args[1])
+
 	pT := pascalsTriangle(n)
+	// n choose k : pT[n][k]
+
+	kCondition := func(k int, seq []int) bool { 
+		// The zero-th condition is well defined (and should be always verified, a_0 = 0)
+		l := len(seq)
+		if k >= l {
+			log.Fatalf("k >= len(seq) in kCondition (k, seq)",k,seq)
+		}
+		S := 0
+		pow := 1
+		for j := range(k+1) {
+			S += pT[k][seq[j]]
+			// fmt.Println(k,seq[j],pT[k][seq[j]])
+			pow *= 2
+		}
+		pow /= 2
+		// fmt.Println(pow,S)
+		return S == pow
+	}
+	
+	worksUntil := func(seq []int) int {
+		// Returns the first index of seq that makes the sequence fail to check the conditions. 
+		// Returns len(seq) (last index + 1) if cheks all conditions.
+		k := 0
+		for (k < len(seq) && kCondition(k, seq)) {
+			k++
+		}
+		return k
+	}
+
+	seq := make([]int, n)	
+
+	for worksUntil(seq) < n {
+		seq[worksUntil(seq)]++
+		
+	}
+	fmt.Println(seq)
+	fmt.Println(worksUntil(seq) == n)
 }
